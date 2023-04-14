be769828de4cedaa214913848f6b201cf19ff910
alesilva <alexandre.silva@nokia.com>
2023-04-14 15:17:34 +0100

    first commit

---
+++ src/main/java/com/nokia/nsw/uiv/splitter/util/Config.java
@@ -0,0 +1,10 @@
+package com.bigid.util;
+
+public class Config {
+
+    public final static Integer LINES_AMOUNT = 1000;
+    public final static String PATH_FILE = "src/main/resources/big.txt";
+    public final static String[] SEARCH_TOKEN = {"James","John","Robert","Michael","William","David","Richard","Charles","Joseph","Thomas","Christopher","Daniel","Paul","Mark",
+            "Donald","George","Kenneth","Steven","Edward","Brian","Ronald","Anthony","Kevin","Jason","Matthew","Gary","Timothy","Jose","Larry","Jeffrey","Frank","Scott","Eric",
+            "Stephen","Andrew","Raymond","Gregory","Joshua","Jerry","Dennis","Walter","Patrick","Peter","Harold","Douglas","Henry","Carl","Arthur","Ryan","Roger"};
+}
---
+++ src/main/java/com/nokia/nsw/uiv/splitter/util/FileSplitterApplication.java
@@ -0,0 +1,11 @@
+package com.nokia.nsw.uiv.splitter.util;
+
+import com.nokia.nsw.uiv.splitter.module.processdata.FileSplitter;
+
+public class BigIdApplication {
+
+    public static void main(String[] args) {
+        FileSplitter fileSplitter = new FileSplitter();
+        fileSplitter.readText();
+    }
+}
